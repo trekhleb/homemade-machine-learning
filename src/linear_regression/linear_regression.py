571c1097815b34da05735a10f8a73b0a60bbda5a
Oleksii Trekhleb <trehleb@gmail.com>
2018-11-20 16:52:37 +0200

    Provide linear regression demo.

--- src/linear_regression/linear_regression.py
+++ src/linear_regression/linear_regression.py
@@ -15,6 +15,7 @@ class LinearRegression:
 
         # Calculate the number of training examples and features.
         num_examples = training_set.shape[0]
+        num_features = training_set.shape[1]
 
         # Normalize features.
         (
@@ -31,6 +32,9 @@ class LinearRegression:
         self.features_mean = features_mean
         self.features_deviation = features_deviation
 
+        # Initialize model parameters.
+        self.theta = np.zeros((num_features + 1, 1))
+
     def train(self, alpha, lambda_param, num_iterations):
         """Trains linear regression.
 
@@ -39,29 +43,17 @@ class LinearRegression:
         :param num_iterations: number of gradient descent iterations.
         """
 
-        # Calculate the number of training examples and features.
-        num_features = self.training_set.shape[1]
-
-        # Initialize model parameters.
-        initial_theta = np.zeros((num_features, 1))
-
         # Run gradient descent.
-        (theta, cost_history) = self.gradient_descent(
-            initial_theta,
-            alpha,
-            lambda_param,
-            num_iterations
-        )
+        cost_history = self.gradient_descent(alpha, lambda_param, num_iterations)
 
-        return theta, self.features_mean, self.features_deviation, self.training_set, cost_history
+        return self.theta, cost_history
 
-    def gradient_descent(self, theta, alpha, lambda_param, num_iterations):
+    def gradient_descent(self, alpha, lambda_param, num_iterations):
         """Gradient descent.
 
         It calculates what steps (deltas) should be taken for each theta parameter in
         order to minimize the cost function.
 
-        :param theta: parameters of the model.
         :param alpha: learning rate (the size of the step for gradient descent)
         :param lambda_param: regularization parameter
         :param num_iterations: number of gradient descent iterations.
@@ -72,19 +64,18 @@ class LinearRegression:
 
         for iteration in range(num_iterations):
             # Perform a single gradient step on the parameter vector theta.
-            theta = self.gradient_step(theta, alpha, lambda_param)
+            self.gradient_step(alpha, lambda_param)
 
             # Save the cost J in every iteration.
-            cost_history.append(self.cost_function(theta, lambda_param))
+            cost_history.append(self.cost_function(lambda_param))
 
-        return theta, cost_history
+        return cost_history
 
-    def gradient_step(self, theta, alpha, lambda_param):
+    def gradient_step(self, alpha, lambda_param):
         """Gradient step.
 
         Function performs one step of gradient descent for theta parameters.
 
-        :param theta: parameters of the model.
         :param alpha: learning rate (the size of the step for gradient descent)
         :param lambda_param: regularization parameter
         """
@@ -93,7 +84,7 @@ class LinearRegression:
         num_examples = self.training_set.shape[0]
 
         # Predictions of hypothesis on all m examples.
-        predictions = self.hypothesis(theta)
+        predictions = self.hypothesis()
 
         # The difference between predictions and actual values for all m examples.
         delta = predictions - self.labels
@@ -101,20 +92,21 @@ class LinearRegression:
         # Calculate regularization parameter.
         reg_param = 1 - alpha * lambda_param / num_examples
 
+        # Create theta shortcut.
+        theta = self.theta
+
         # Vectorized version of gradient descent.
         theta = theta * reg_param - alpha * (1 / num_examples) * (delta.T @ self.training_set).T
-
         # We should NOT regularize the parameter theta_zero.
         theta[1] = theta[1] - alpha * (1 / num_examples) * (self.training_set[:, 0].T @ delta).T
 
-        return theta
+        self.theta = theta
 
-    def cost_function(self, theta, lambda_param):
+    def cost_function(self, lambda_param):
         """Cost function.
 
         It shows how accurate our model is based on current model parameters.
 
-        :param theta: parameters of the model.
         :param lambda_param: regularization parameter
         """
 
@@ -122,11 +114,11 @@ class LinearRegression:
         num_examples = self.training_set.shape[0]
 
         # Get the difference between predictions and correct output values.
-        delta = self.hypothesis(theta) - self.labels
+        delta = self.hypothesis() - self.labels
 
         # Calculate regularization parameter.
         # Remember that we should not regularize the parameter theta_zero.
-        theta_cut = theta[1:, 0]
+        theta_cut = self.theta[1:, 0]
         reg_param = lambda_param * (theta_cut.T @ theta_cut)
 
         # Calculate current predictions cost.
@@ -135,17 +127,22 @@ class LinearRegression:
         # Let's extract cost value from the one and only cost numpy matrix cell.
         return cost[0][0]
 
-    def hypothesis(self, theta):
+    def hypothesis(self):
         """Hypothesis function.
 
         It predicts the output values y based on the input values X and model parameters.
 
-        :param theta: parameters of the model.
         :return: predictions made by model based on provided theta.
         """
 
-        predictions = self.training_set @ theta
+        predictions = self.training_set @ self.theta
 
+        return predictions
+
+    def predict(self, data_set):
+        """Predict the output for data_set input based on trained theta values"""
+        (data_set_normalized, mean, deviation) = self.normalize_features(data_set)
+        predictions = data_set_normalized @ self.theta
         return predictions
 
     @staticmethod
