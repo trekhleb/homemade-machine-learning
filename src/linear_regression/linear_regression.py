4dabe4adbcce9a61a5cb3d23d01ee16a6aaa2dce
Oleksii Trekhleb <trehleb@gmail.com>
2018-11-21 05:34:31 +0200

    Refactor linear regression.

--- src/linear_regression/linear_regression.py
+++ src/linear_regression/linear_regression.py
@@ -6,28 +6,28 @@ import numpy as np
 class LinearRegression:
     """Linear Regression Class"""
 
-    def __init__(self, training_set, labels):
+    def __init__(self, data, labels):
         """Linear regression constructor.
 
-        :param training_set: training set.
+        :param data: training set.
         :param labels: training set outputs (correct values).
         """
 
         # Normalize features and add ones column.
         (
-            training_set_normalized,
+            data_processed,
             features_mean,
             features_deviation
-        ) = LinearRegression.prepare_data(training_set)
+        ) = LinearRegression.prepare_data(data)
 
-        self.training_set = training_set_normalized
+        self.data = data_processed
         self.labels = labels
         self.features_mean = features_mean
         self.features_deviation = features_deviation
 
         # Initialize model parameters.
-        num_features = training_set.shape[1]
-        self.theta = np.zeros((num_features + 1, 1))
+        num_features = self.data.shape[1]
+        self.theta = np.zeros((num_features, 1))
 
     def train(self, alpha, lambda_param, num_iterations):
         """Trains linear regression.
@@ -61,7 +61,7 @@ class LinearRegression:
             self.gradient_step(alpha, lambda_param)
 
             # Save the cost J in every iteration.
-            cost_history.append(self.cost_function(self.training_set, self.labels, lambda_param))
+            cost_history.append(self.cost_function(self.data, self.labels, lambda_param))
 
         return cost_history
 
@@ -74,11 +74,11 @@ class LinearRegression:
         :param lambda_param: regularization parameter
         """
 
-        # Calculate the number of training examples and features.
-        num_examples = self.training_set.shape[0]
+        # Calculate the number of training examples.
+        num_examples = self.data.shape[0]
 
         # Predictions of hypothesis on all m examples.
-        predictions = LinearRegression.hypothesis(self.training_set, self.theta)
+        predictions = LinearRegression.hypothesis(self.data, self.theta)
 
         # The difference between predictions and actual values for all m examples.
         delta = predictions - self.labels
@@ -90,27 +90,39 @@ class LinearRegression:
         theta = self.theta
 
         # Vectorized version of gradient descent.
-        theta = theta * reg_param - alpha * (1 / num_examples) * (delta.T @ self.training_set).T
+        theta = theta * reg_param - alpha * (1 / num_examples) * (delta.T @ self.data).T
         # We should NOT regularize the parameter theta_zero.
-        theta[1] = theta[1] - alpha * (1 / num_examples) * (self.training_set[:, 0].T @ delta).T
+        theta[1] = theta[1] - alpha * (1 / num_examples) * (self.data[:, 0].T @ delta).T
 
         self.theta = theta
 
-    def cost_function(self, data_set, labels, lambda_param):
+    def get_cost(self, data, labels, lambda_param):
+        """Get the cost value for specific data set.
+
+        :param data: the set of training or test data.
+        :param labels: training set outputs (correct values).
+        :param lambda_param: regularization parameter
+        """
+
+        data_processed = LinearRegression.prepare_data(data)[0]
+
+        return self.cost_function(data_processed, labels, lambda_param)
+
+    def cost_function(self, data, labels, lambda_param):
         """Cost function.
 
         It shows how accurate our model is based on current model parameters.
 
-        :param data_set: the set of training or test data.
+        :param data: the set of training or test data.
         :param labels: training set outputs (correct values).
         :param lambda_param: regularization parameter
         """
 
         # Calculate the number of training examples and features.
-        num_examples = data_set.shape[0]
+        num_examples = data.shape[0]
 
         # Get the difference between predictions and correct output values.
-        delta = LinearRegression.hypothesis(data_set, self.theta) - labels
+        delta = LinearRegression.hypothesis(data, self.theta) - labels
 
         # Calculate regularization parameter.
         # Remember that we should not regularize the parameter theta_zero.
@@ -130,10 +142,10 @@ class LinearRegression:
         """
 
         # Normalize features and add ones column.
-        data_normalized = LinearRegression.prepare_data(data)[0]
+        data_processed = LinearRegression.prepare_data(data)[0]
 
         # Do predictions using model hypothesis.
-        predictions = LinearRegression.hypothesis(data_normalized, self.theta)
+        predictions = LinearRegression.hypothesis(data_processed, self.theta)
 
         return predictions
 
@@ -167,9 +179,9 @@ class LinearRegression:
         ) = LinearRegression.normalize_features(data)
 
         # Add a column of ones to X.
-        data_normalized = np.hstack((np.ones((num_examples, 1)), data_normalized))
+        data_processed = np.hstack((np.ones((num_examples, 1)), data_normalized))
 
-        return data_normalized, features_mean, features_deviation
+        return data_processed, features_mean, features_deviation
 
     @staticmethod
     def normalize_features(data):
